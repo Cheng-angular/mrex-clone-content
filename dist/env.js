6717136a01067401a032c303a74ff022daa74a29
Cheng-angular <angulardev0811@outlook.com>
2020-12-06 08:20:14 -0800

    update script for language search in url

---
+++ dist/env.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment1 = exports.environment = void 0;
+exports.environment = {
+    production: false,
+    serverUrl: 'https://mrex.co/en/the-complete-guide-to-multifamily-real-estate-investing/'
+};
+exports.environment1 = {
+    production: false,
+    serverUrl: 'https://mrex.co/fr/guide-complet-de-linvestissement-en-immobilier-multilogements/'
+};
+//# sourceMappingURL=env.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,11 @@ import cheerio from 'cheerio';
 import { environment, environment1 } from './env';
 
 var fs = require('fs');
-const targeturl = environment.serverUrl;
-const eng = 'en';
-const french = 'fr';
-// let inputfile = () => {
-//     if (targeturl.includes(eng)) {         
-//        return filename = "en.json";
-//     } else if(targeturl.includes(french)) {
-//         return filename = "fr.json";                
-//     }
-// }
+const targeturl = environment1.serverUrl;
+const eng = '/en/';
+const french = '/fr/';
+let inputfile = '';
+let language = '';
 const AxiosInstance = axios.create();
 let articleData: Articles[] = [];
 
@@ -29,47 +24,57 @@ interface Articles {
     thumbnailUrl: string;
 }
 
-AxiosInstance.get(targeturl)
-.then(
-    response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const thumbnail = $('.list_articles > li > a');        
-        thumbnail.each((i, elem) => {
-            const thumbnailUrl = $(elem).attr().href;      
-            AxiosInstance.get(thumbnailUrl)
-            .then(
-                response => {
-                    const htmlcontent = response.data;
-                    const $ = cheerio.load(htmlcontent);
-                    const content = $('#page').html();
-                    const title = $(content).find('#single_hero_content_title').text().trim();
-                    const summary = $(content).find('#single_post_excerpt > p').text();
-                    const author = $(content).find('#single_hero_content_pre_title').text().slice(3);
-                    const authorDescription = $(content).find('#single_post_content > p:last-child').text();
-                    const htmlContent = $('#single_post_content_container').children().html();
-                    const publishDate = $(content).find('#single_hero_content_surtitle').text();
-                    const authorPicture = $(content).find('#single_post_author_pic').attr().style.slice(24, -3);
-                    articleData.push({
-                        title: title,
-                        summary: summary,
-                        author: author,
-                        authorDescription: authorDescription,
-                        htmlContent: htmlContent,
-                        publishDate: publishDate,
-                        language: 'en',
-                        publicUrl: targeturl,
-                        authorPicture: authorPicture,
-                        thumbnailUrl: thumbnailUrl
-                    });                    
-                    fs.writeFile ('input.json', JSON.stringify(articleData), function(err){
-                        if(err) throw err;
-                    })
-                }
-            )
-            
-        });
-        
-    }
-).catch(console.error);
+(async () => {    
+    try {
+            if (targeturl.includes(eng)) {         
+               inputfile = "en.json";
+               language = "english";
+            } else if(targeturl.includes(french)) {                     
+                inputfile = "fr.json";
+                language = "French";                
+            }
+            const response = await axios.get(targeturl);
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const thumbnail = $('.list_articles > li > a');      
+            thumbnail.each((i,elem) => {
+                const thumbnailUrl = $(elem).attr().href;      
+                AxiosInstance.get(thumbnailUrl)
+                .then(
+                    response => {
+                        const htmlcontent = response.data;
+                        const $ = cheerio.load(htmlcontent);
+                        const content = $('#page').html();
+                        const title = $(content).find('#single_hero_content_title').text().trim();
+                        const summary = $(content).find('#single_post_excerpt > p').text();
+                        const author = $(content).find('#single_hero_content_pre_title').text().slice(3);
+                        const authorDescription = $(content).find('#single_post_content > p:last-child').text();
+                        const htmlContent = $('#single_post_content_container').children().html();
+                        const publishDate = $(content).find('#single_hero_content_surtitle').text();
+                        const authorPicture = $(content).find('#single_post_author_pic').attr().style.slice(24, -3);
+                        articleData.push({
+                            title: title,
+                            summary: summary,
+                            author: author,
+                            authorDescription: authorDescription,
+                            htmlContent: htmlContent,
+                            publishDate: publishDate,
+                            language: language,
+                            publicUrl: targeturl,
+                            authorPicture: authorPicture,
+                            thumbnailUrl: thumbnailUrl
+                        });                    
+                        fs.writeFile (inputfile, JSON.stringify(articleData), function(err){
+                            if(err) throw err;
+                        })
+                    }
+                )
+            });
+        } 
+        catch (error) {
+            console.log(error);
+        }
+})();
+
+
 
